8ed51a764c1efa37ba8b8d462a0ea286fe9f4c5b
Adam Hansrod <adamhansrod@gmail.com>
2017-09-02 21:02:16 +0100

    Base our image on alpine-java, that has oracle java installed rather than openjdk This is only java specific for our sample app, we could just use alpine and use something like the heroku buildpacks

--- app-base/scripts/startup.sh
+++ app-base/scripts/startup.sh
@@ -1,14 +1,24 @@
 #!/bin/sh
 
-$app_name = $1
+set -x
 
-curl -O /app-config/$app_name.config repository.service/config/$app_name/$app_name.config
-awk '{print "export " $0}' $app_name.config
-source $app_name.config
+app_name=$1
+repository_service=$2
 
-mkdir /app
-curl -O /app-files/$app_name.tgz repository.service/files/$app_name/$app_name.tgz
+cd /opt/app
 
-tar -C /app -xzvf /app-files/$app_name.tgz
-cd /app
-$(STARTUP_COMMAND)
+echo "Downloading app config: ${repository_service}/config/${app_name}/${app_name}.config"
+curl ${repository_service}/config/${app_name}/${app_name}.config -o config/${app_name}.config
+awk '{print "export " $0}' config/${app_name}.config
+cat config/${app_name}.config
+source config/${app_name}.config
+
+echo "Downloading app package: ${repository_service}/files/${app_name}/${app_name}.tgz"
+curl ${repository_service}/files/${app_name}/${app_name}.tgz -o source/${app_name}.tgz
+
+tar -C /opt/app/files -xzvf source/${app_name}.tgz
+
+echo "Running startup command: ${STARTUP_COMMAND}"
+cd /opt/app/files
+
+exec $STARTUP_COMMAND

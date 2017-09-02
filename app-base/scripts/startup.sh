1a7c1bad1098c2f5b5aabbdbcbd6a2f73d4ec2c6
Adam Hansrod <adamhansrod@gmail.com>
2017-09-02 18:45:38 +0100

    Add repository files, and changes to app-base to enable it to pull down apps on startup

--- app-base/scripts/startup.sh
+++ app-base/scripts/startup.sh
@@ -1,1 +1,14 @@
-echo "Hello world"
+#!/bin/sh
+
+$app_name = $1
+
+curl -O /app-config/$app_name.config repository.service/config/$app_name/$app_name.config
+awk '{print "export " $0}' $app_name.config
+source $app_name.config
+
+mkdir /app
+curl -O /app-files/$app_name.tgz repository.service/files/$app_name/$app_name.tgz
+
+tar -C /app -xzvf /app-files/$app_name.tgz
+cd /app
+$(STARTUP_COMMAND)
